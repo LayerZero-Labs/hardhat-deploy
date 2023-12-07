6e4555abccace7cf73af1e3903c3f4efd040d5e8
Elias Rad <146735585+nnsW3@users.noreply.github.com>
2023-12-07 23:15:30 +0200

    fix typos ProxyAdmin.sol

--- solc_0.8/openzeppelin/proxy/transparent/ProxyAdmin.sol
+++ solc_0.8/openzeppelin/proxy/transparent/ProxyAdmin.sol
@@ -22,7 +22,7 @@ contract ProxyAdmin is Ownable {
      * - This contract must be the admin of `proxy`.
      */
     function getProxyImplementation(TransparentUpgradeableProxy proxy) public view virtual returns (address) {
-        // We need to manually run the static call since the getter cannot be flagged as view
+        // We need to manually run the static call since the getter cannot be flagged as a view
         // bytes4(keccak256("implementation()")) == 0x5c60da1b
         (bool success, bytes memory returndata) = address(proxy).staticcall(hex"5c60da1b");
         require(success);
@@ -37,7 +37,7 @@ contract ProxyAdmin is Ownable {
      * - This contract must be the admin of `proxy`.
      */
     function getProxyAdmin(TransparentUpgradeableProxy proxy) public view virtual returns (address) {
-        // We need to manually run the static call since the getter cannot be flagged as view
+        // We need to manually run the static call since the getter cannot be flagged as a view
         // bytes4(keccak256("admin()")) == 0xf851a440
         (bool success, bytes memory returndata) = address(proxy).staticcall(hex"f851a440");
         require(success);
