585f2696260b5e4ef02cff64f70dd1ede3199da5
aurel-fr <105201452+aurel-fr@users.noreply.github.com>
2024-01-19 12:03:47 +0100

    test: tron config and getDefaultArtifact

--- test/project.test.ts
+++ test/project.test.ts
@@ -1,6 +1,10 @@
-import {assert} from 'chai';
+import {assert, expect} from 'chai';
 
 import {useEnvironment} from './helpers';
+import {getDefaultArtifact} from '../src/defaultArtifacts';
+
+import {ProxyAdmin as regularProxyAdmin} from '../extendedArtifacts';
+import {ProxyAdmin as tronProxyAdmin} from '../extendedArtifactsTron';
 
 describe('hardhat-deploy hre extension', function () {
   useEnvironment('hardhat-project', 'hardhat');
@@ -10,5 +14,30 @@ describe('hardhat-deploy hre extension', function () {
 
   it('The getChainId should give the correct chainId', async function () {
     assert.equal(await this.env.getChainId(), '31337');
+  });
+});
+
+describe(`${getDefaultArtifact.name} function tests`, function () {
+  it('should return a regular default artifact if the tron param is false', function () {
+    const artifact = getDefaultArtifact('DefaultProxyAdmin', false);
+    expect(artifact).to.equal(regularProxyAdmin);
+  });
+  it('should return a tron default artifact if the tron param is true', function () {
+    const artifact = getDefaultArtifact('DefaultProxyAdmin', true);
+    expect(artifact).to.equal(tronProxyAdmin);
+  });
+});
+
+describe('@layerzerolabs/hardhat-deploy with a non-tron network', function () {
+  useEnvironment('hardhat-project', 'hardhat');
+  it('should not set the tron boolean in the network', function () {
+    expect(this.env.network.tron).to.be.undefined;
+  });
+});
+
+describe('@layerzerolabs/hardhat-deploy with a tron network', function () {
+  useEnvironment('hardhat-project', 'tron');
+  it('It should set the tron boolean in the network', function () {
+    expect(this.env.network.tron).to.be.true;
   });
 });
